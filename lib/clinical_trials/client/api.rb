971c49bf3cb5335b26ffa87edddcad366f37a5a3
blakeshall <blake@blakeshall.com>
2011-06-22 12:48:54 -0700

    Added search method and fixed its test

--- lib/clinical_trials/client/api.rb
+++ lib/clinical_trials/client/api.rb
@@ -2,6 +2,10 @@ module ClinicalTrials
   class Client
     module Api
       
+      def search(term)
+        term.sub(/\s/, "+")
+        get("search?term=%22#{term}%22&displayxml=true")
+      end
     end
   end
 end
--- spec/clinical_trials/api_spec.rb
+++ spec/clinical_trials/api_spec.rb
@@ -15,7 +15,7 @@ describe ClinicalTrials::Client::Api do
     it "should return the correct item" do
       test = @client.search("lyme disease")
       a_get("search?term=%22lyme+disease%22&displayxml=true").should have_been_made
-      test.clinical_study.order.should == 1
+      test.search_results.clinical_study[0].order.should == "1"
     end
   end
   
