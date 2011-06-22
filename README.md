734204f97df986bdeff0e113c6e977ae4dd9f336
blakeshall <blake@blakeshall.com>
2011-06-22 12:58:04 -0700

    Wrote README and added documentation to search method

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# The Code for America ApiRubyGem template
+# The Clinical Trails search gem
+Allows access to the clinicaltrails.gov search api
+
+Usage
+-----
+	require 'clinical_trials'
+	
+	ClinicalTrials.search("lyme disease")
+	#Returns results as a hash
 
 [![Code for America Tracker](http://stats.codeforamerica.org/codeforamerica/clinical_trials.png)](http://stats.codeforamerica.org/projects/clinical_trials)
--- lib/clinical_trials/client/api.rb
+++ lib/clinical_trials/client/api.rb
@@ -2,9 +2,17 @@ module ClinicalTrials
   class Client
     module Api
       
-      def search(term)
+      # Returns search results for given term
+      # @param term [String] The term to search for
+      # @param options [Hash] A customizable set of options. 
+      # @return {Hash}
+      # @see http://clinicaltrials.gov/ct2/info/linking
+      # @example
+      #   searc("lyme disease")
+      
+      def search(term, options={})
         term.sub(/\s/, "+")
-        get("search?term=%22#{term}%22&displayxml=true")
+        get("search?term=%22#{term}%22&displayxml=true", options)
       end
     end
   end
