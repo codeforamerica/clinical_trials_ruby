d3f2ff61b7f13b2c4bb4685ae2827b52df5e5666
blakeshall <blake@blakeshall.com>
2011-06-22 12:43:46 -0700

    Added fixture, fixed URLs in helper.rb, and add search spec

--- spec/clinical_trials/api_spec.rb
+++ spec/clinical_trials/api_spec.rb
@@ -6,4 +6,17 @@ describe ClinicalTrials::Client::Api do
     @client = ClinicalTrials::Client.new
   end
   
+  describe ".search" do
+    before do
+      stub_get("search?term=%22lyme+disease%22&displayxml=true").
+        to_return(:status => 200, :body => fixture("search.xml"))
+    end
+      
+    it "should return the correct item" do
+      test = @client.search("lyme disease")
+      a_get("search?term=%22lyme+disease%22&displayxml=true").should have_been_made
+      test.clinical_study.order.should == 1
+    end
+  end
+  
 end
